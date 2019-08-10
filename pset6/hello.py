e4506d99287775490c623169c610129aeeb4676c
Chuxin Huang <chuxin@canva.com>
2019-08-11 09:37:33 +1000

    pset6/hello.py

---
+++ pset6/hello.py
@@ -0,0 +1,16 @@
+'''
+cs50-2019 pset6/hello
+
+The code is used to achieve the following:
+
+$ python hello.py
+What is your name?
+David
+hello, David
+
+'''
+
+from cs50 import get_float
+
+name = get_string("What is your name?")
+print(f"Hello, {name}")
