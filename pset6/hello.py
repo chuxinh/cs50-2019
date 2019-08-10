c43e5082a815e55c2a304da68f7ffa76e8afea81
Chuxin Huang <chuxin@canva.com>
2019-08-11 09:44:16 +1000

    add mario

--- pset6/hello.py
+++ pset6/hello.py
@@ -1,15 +1,9 @@
-"""
-cs50-2019 pset6/hello
-
-The code is used to achieve the following:
-
+"""The code is used to achieve the following:
 $ python hello.py
 What is your name?
 David
 hello, David
-
 """
-
 from cs50 import get_float
 
 name = get_string("What is your name?")
